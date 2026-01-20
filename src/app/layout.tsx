e1335d6a256cce058854bdb9047883fecc8d4cce
Kr-Sahil-Works <sahillearn44@gmail.com>
2026-01-21 01:44:44 +0530

    2- convex db basics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "../providers/theme-provider";
+import { ConvexClientProvider } from "@/providers/convex-client-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <ConvexClientProvider>
           {children}
+          </ConvexClientProvider>
         </ThemeProvider>
       </body>
     </html>
