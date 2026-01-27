825600c821f6564c170960d297cf62fa12ed76f2
Kr-Sahil-Works <sahillearn44@gmail.com>
2026-01-27 15:41:43 +0530

    3 - getmessages query added

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 
 export const sendTextMessage = mutation({
@@ -44,3 +44,81 @@ export const sendTextMessage = mutation({
         // todo => add chatgpt later 
     }
 })
+
+     //  --------- # Optimized Version Of Function
+    export const getMessages = query({
+        args:{
+            conversation: v.id("conversations"),
+        },handler: async (ctx,args) => {
+            const identity = await ctx.auth.getUserIdentity();
+            if(!identity) {
+                throw new ConvexError("Unauthorized")
+            }
+
+            const messages = await ctx.db
+            .query("messages")
+            .withIndex("by_conversation",(q)=>q.eq("conversation",args.conversation))
+            .collect();
+
+            // Stores the new array of users no duplicates
+            const userProfileCache = new Map();
+
+            const messagesWithSender = await Promise.all(
+                // Creates the new changed array for messageSender users
+                messages.map(async(message) => {
+                    let sender;
+                    // Check if sender profile is in cache
+                    if (userProfileCache.has(message.sender)){
+                        sender = userProfileCache.get(message.sender)
+                    } else {
+                        // Fetch sender profile from the database
+                        sender = await ctx.db
+                        .query("users")
+                        .filter((q) => q.eq(q.field("_id"), message.sender))
+                        .first();
+                        // Cache the sender profile
+                        userProfileCache.set(message.sender ,sender); 
+                    }
+
+                    return {...message, sender};
+                })
+            );
+
+            return messagesWithSender;
+        }
+    })
+
+
+
+
+    //  --------- # Unoptimized Version Of Function
+// export const getMessages = query({
+//     args:{
+//         conversation: v.id("conversations"),
+//     },
+//     handler : async (ctx,args) => {
+//         const identity = await ctx.auth.getUserIdentity();
+//         if(!identity) {
+//             throw new ConvexError("Not authenticated") 
+//         }
+
+//         const messages = await ctx.db
+//         .query("messages")
+//         .withIndex("by_conversation", q=> q.eq("conversation", args.conversation))
+//         .collect();
+
+
+//         const messagesWithSender = await Promise.all(
+//             messages.map(async (message) => {
+//                 const sender = await ctx.db
+//                 .query("users")
+//                 .filter(q => q.eq(q.field("_id"), message.sender))
+//                 .first();
+
+//                 return{...message,sender}
+//             })
+//         ) 
+
+//         return messagesWithSender;
+//     }
+// })
--- src/components/home/message-container.tsx
+++ src/components/home/message-container.tsx
@@ -1,7 +1,15 @@
-import { messages } from "@/dummy-data/db";
 import ChatBubble from "./chat-bubble";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useConversationStore } from "@/store/chat-store";
 
 const MessageContainer = () => {
+  const { selectedConversation } = useConversationStore();
+  const messages = useQuery(api.messages.getMessages,{
+    conversation : selectedConversation!._id,
+  })
+  const me = useQuery(api.users.getMe);
+  
   return (
     <div
       className="
@@ -13,9 +21,13 @@ const MessageContainer = () => {
       "
     >
       <div className="mx-12 flex flex-col gap-3 min-h-full">
-        {messages?.map((msg) => (
+        {messages?.map((msg,idx) => (
           <div key={msg._id}>
-            <ChatBubble />
+            <ChatBubble 
+            me ={me}
+            message={msg}
+            previousMessage = {idx > 0 ? messages [idx -1] : undefined}
+            />
           </div>
         ))}
       </div>
